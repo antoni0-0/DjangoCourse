1b4fc2b16d87121cd9c86f3194838d9e1df769a4
Antonio Carbonó Pedroza <antonio.carbonop@gmail.com>
2025-02-18 20:58:08 -0500

    feat: se agrega taller 3

--- helloworld/pages/models.py
+++ helloworld/pages/models.py
@@ -8,5 +8,5 @@ class Product(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     
 class Comment(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='comments', on_delete=models.CASCADE)
     description = models.TextField()
--- helloworld/pages/views.py
+++ helloworld/pages/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django import forms
 from .models import Product
+from .utils import ImageLocalStorage
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
@@ -37,13 +38,16 @@ class ProductShowView(View):
             product_id = int(id)
             if product_id < 1:
                 raise ValueError("Product id must be 1 or greater")
-            product = get_object_or_404(Product, pk=product_id)
+            # product = get_object_or_404(Product, pk=product_id)
         except (ValueError, IndexError):
             return HttpResponseRedirect(reverse('home'))
+        
         viewData = {}
+        product = get_object_or_404(Product, pk=product_id)
         viewData["title"] = product.name + " - Online Store"
         viewData["subtitle"] = product.name + " - Product information"
         viewData["product"] = product
+        
         return render(request, self.template_name, viewData)
     
 class ProductForm(forms.ModelForm):
@@ -74,10 +78,65 @@ class ProductCreateView(View):
 class ProductListView(ListView):
     model = Product
     template_name = 'product_list.html'
-    context_object_name = 'products' # This will allow you to loop through 'products' in your template
+    context_object_name = 'products'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Products - Online Store'
         context['subtitle'] = 'List of products'
         return context 
+    
+class CartView(View):
+    template_name = 'cart/index.html'
+    def get(self, request):
+        products = {}
+        products[121] = {'name': 'Tv samsung', 'price': '1000'}
+        products[11] = {'name': 'Iphone', 'price': '2000'}
+        cart_products = {}
+        cart_product_data = request.session.get('cart_product_data', {})
+        for key, product in products.items():
+            if str(key) in cart_product_data.keys():
+                cart_products[key] = product
+            view_data = {
+                'title': 'Cart - Online Store',
+                'subtitle': 'Shopping Cart',
+                'products': products,
+                'cart_products': cart_products
+                }
+        return render(request, self.template_name, view_data)
+    def post(self, request, product_id):
+        cart_product_data = request.session.get('cart_product_data', {})
+        cart_product_data[product_id] = product_id
+        request.session['cart_product_data'] = cart_product_data
+        return redirect('cart_index')
+
+class CartRemoveAllView(View):
+    def post(self, request):
+        if 'cart_product_data' in request.session:
+            del request.session['cart_product_data']
+        return redirect('cart_index')
+    
+def ImageViewFactory(image_storage):
+    class ImageView(View):
+        template_name = 'images/index.html'
+
+        def get(self, request):
+            image_url = request.session.get('image_url', '')
+            return render(request, self.template_name, {'image_url': image_url})
+
+        def post(self, request):
+            image_url = image_storage.store(request)
+            request.session['image_url'] = image_url
+            return redirect('image_index')
+    return ImageView 
+
+class ImageViewNoDI(View):
+    template_name = 'images/index.html'
+    def get(self, request):
+        image_url = request.session.get('image_url', '')
+        return render(request, self.template_name, {'image_url': image_url})
+    def post(self, request):
+        image_storage = ImageLocalStorage()
+        image_url = image_storage.store(request)
+        request.session['image_url'] = image_url
+        return redirect('image_index')
